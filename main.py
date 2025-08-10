3e9c1453e8ffa7145f672ef217af43a4c2ab01cd
mulandi <mulandimorris1@gmail.com>
2025-08-10 23:19:05 +0300

    updating rows and columns

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ symbol_count = {
     "D": 8
 }
 
+def get_slot_machine_spin(rows, cols, symbols): 
+    all_symbols = [] 
+    for symbol, symbol_count in symbols.items(): 
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    columns = [[], [], []]
+
 def deposit(): 
     while True: 
         amount = input("What would you like to deposit? Ksh.")
