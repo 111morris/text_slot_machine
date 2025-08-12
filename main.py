55f54954ff8e41b7ddecf4473f4ecbc5ecc5cf18
mulandi <mulandimorris1@gmail.com>
2025-08-12 23:14:33 +0300

    dvpkg

--- main.py
+++ main.py
@@ -6,32 +6,37 @@ ROWS = 3
 COLS = 3
 
 symbol_count = {
-    "A": 2, 
-    "B": 4, 
-    "C": 6, 
-    "D": 8
+ "A": 2, 
+ "B": 4, 
+ "C": 6, 
+ "D": 8
 }
 
-def get_slot_machine_spin(rows, cols, symbols): 
-    all_symbols = [] 
-    for symbol, symbol_count in symbols.items(): 
-        for _ in range(symbol_count):
-            all_symbols.append(symbol)
-    columns = [[], [], []]
+def get_slot_machine_spin(rows, cols, symbols):
+ all_symbols = [] 
+ for symbol, symbol_count in symbols.items(): 
+  for _ in range(symbol_count):
+   all_symbols.append(symbol)
+   columns = []
+   for col in range(cols): 
+    column = []
+    current_symbols = all_symbols[:]
+    for row in range(rows):
+     value = random.choice(all_symbols)
 
 def deposit(): 
-    while True: 
-        amount = input("What would you like to deposit? Ksh.")
-        if amount.isdigit(): 
-            amount = int(amount)
-            if amount > 0:
-                break
-            else: 
-                print("Amount must be greater than 0.")
-        else: 
-            print('Please enter a number.')
-
-    return amount
+ while True: 
+  amount = input("What would you like to deposit? Ksh.")
+  if amount.isdigit(): 
+   amount = int(amount)
+   if amount > 0:
+    break
+   else:
+    print("Amount must be greater than 0.")
+  else:
+   print('Please enter a number.')
+ return amount
+ 
 def get_number_of_lines(): 
     while True: 
         lines = input("Enter the number of lines (1-"+str(MAX_LINES) + "): ")
